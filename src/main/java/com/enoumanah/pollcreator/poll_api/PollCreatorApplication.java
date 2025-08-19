46a3ab3cc51f1f1b1c51761534dd3a3118001646
Eno Abasi Emmanuel Umanah <141334494+enoumanah@users.noreply.github.com>
2025-08-19 12:12:22 +0100

    Initial Commit

---
+++ src/main/java/com/enoumanah/pollcreator/poll_api/PollCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.enoumanah.pollcreator.poll_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enoumanah/pollcreator/poll_api/PollCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enoumanah.pollcreator.poll_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
