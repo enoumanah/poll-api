256c757d5c4c8c0396a89076a7f878b5fcf6a6f1
enoumanah <141334494+enoumanah@users.noreply.github.com>
2025-09-22 22:31:39 +0100

    Update PollController.java

--- src/main/java/com/enoumanah/pollcreator/poll_api/controller/PollController.java
+++ src/main/java/com/enoumanah/pollcreator/poll_api/controller/PollController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*") // Update to Vercel URL in prod
 @RestController
 @RequestMapping("/api/polls")
 public class PollController {
